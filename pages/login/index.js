39764ac57a2cd74ac7da78741e0973d1a00026cf
donalawa <awadonalcien12@gmail.com>
2022-10-02 01:41:30 +0100

    Update

---
+++ pages/login/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './login.module.css'
+
+const login = () => {
+    return (
+        <div>
+            <h1 className={styles.title}>Login Page</h1>
+            
+        </div>
+    )
+
+}
+
+export default login;
